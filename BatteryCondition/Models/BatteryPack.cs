494022a713c377228e6003e6791883c78fd6be7a
Ignis Infernalis <ignis.infernalis@outlook.com>
2018-06-21 12:53:54 +0300

    Add class BatteryPack.

---
+++ BatteryCondition/Models/BatteryPack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatteryCondition.Models
+{
+    public class BatteryPack
+    {
+        public int BatteryPackId { get; set; }
+        public AddressByDate AddressByDates { get; set; }
+        public List<BatteryCondition> BatteryConditions { get; set; }
+    }
+}
