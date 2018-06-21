49beeda4c42459a97cf6ad312dbefff72125ac3e
Ignis Infernalis <ignis.infernalis@outlook.com>
2018-06-21 15:25:59 +0300

    Add Local Id for BatteryCondition

--- BatteryCondition/Models/BatteryCondition.cs
+++ BatteryCondition/Models/BatteryCondition.cs
@@ -8,6 +8,7 @@ namespace BatteryCondition.Models
     public class BatteryCondition
     {
         public int BatteryConditionId { get; set; }
+        public int BatteryLocalId { get; set; }
         public BatteryModel BatteryModel { get; set; }
         public List<AddressByDate> AddressByDates { get; set; }
         public List<CapacityByDate> CapacityByDates { get; set; }
