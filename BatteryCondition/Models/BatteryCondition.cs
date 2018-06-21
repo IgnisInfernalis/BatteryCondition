68032f372289e1dcff19ad9ff01e8f6c90490bde
Ignis Infernalis <ignis.infernalis@outlook.com>
2018-06-22 00:17:34 +0300

    Update DB

--- BatteryCondition/Models/BatteryCondition.cs
+++ BatteryCondition/Models/BatteryCondition.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BatteryCondition.Models
 {
@@ -10,6 +11,7 @@ namespace BatteryCondition.Models
         public int BatteryConditionId { get; set; }
         public int BatteryLocalId { get; set; }
         public BatteryModel BatteryModel { get; set; }
+        public bool BatteryPack { get; set; }
         public List<AddressByDate> AddressByDates { get; set; }
         public List<CapacityByDate> CapacityByDates { get; set; }
     }
--- BatteryCondition/Models/CapacityByDate.cs
+++ BatteryCondition/Models/CapacityByDate.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BatteryCondition.Models
 {
+    [NotMapped]
     public class CapacityByDate
     {
         public int CapacityByDateId { get; set; }
